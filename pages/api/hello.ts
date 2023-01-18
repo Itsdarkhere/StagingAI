b2d2e77f0b4eaa40c3daea93da0babb86854d43d
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-01-18 19:19:54 +0200

    patch 2

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
 
 type Data = {
   name: string;
 };
 
 export default function handler(
-  req: NextApiRequest,
+  req: NextRequest,
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: 'John Doe' });
