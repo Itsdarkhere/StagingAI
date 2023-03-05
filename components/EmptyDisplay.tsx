af231118c4bb1c0214000385632b62a69950919c
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-03-05 21:49:03 +0200

    New component and prettier

---
+++ components/EmptyDisplay.tsx
@@ -0,0 +1,42 @@
+import React, { createRef, useEffect, useState } from 'react';
+import styles from '../styles/StagingDisplay.module.css';
+import lottie from 'lottie-web';
+import LOTTIE from '../public/lottie2.json';
+
+export default function EmptyDisplay() {
+  const [showLottie, setShowLottie] = useState<boolean>(false);
+  let animationContainer = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current!,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: LOTTIE,
+    });
+
+    const lottieShow = () => {
+      setShowLottie(true);
+    };
+
+    anim.addEventListener('DOMLoaded', lottieShow);
+
+    return () => {
+      anim.destroy();
+    };
+  }, [animationContainer]);
+  return (
+    <div className={styles.displayAnimation}>
+      <div ref={animationContainer}></div>
+      <div className={`${styles.pitch} ${showLottie && styles.pitchVisible}`}>
+        Experience the power of AI-generated virtual staging images, and save up
+        to 95% compared to traditional virtual staging agencies who bill up to
+        $60 per image and may take up to 3 days to complete the work. With our
+        AI technology, you&apos;ll receive high-quality virtual staging images
+        in seconds at a fraction of the cost. Don&apos;t wait - start saving
+        time and money today!
+      </div>
+    </div>
+  );
+}
--- components/StagingDisplay.tsx
+++ components/StagingDisplay.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-import React, { createRef, RefObject, useEffect, useState } from 'react';
+import React, { RefObject, useState } from 'react';
 import styles from '../styles/StagingDisplay.module.css';
 import NewRender from './NewRender';
 import Modal from 'react-modal';
 import Sketch from './Sketch';
-import lottie from 'lottie-web';
-import LOTTIE from '../public/lottie2.json';
+import EmptyDisplay from './EmptyDisplay';
 
 export default function StagingDisplay({
   sketchRef,
@@ -25,28 +24,6 @@ export default function StagingDisplay({
 }) {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [modalIMG, setModalIMG] = useState<string>('');
-  const [showLottie, setShowLottie] = useState<boolean>(false);
-  let animationContainer = createRef<HTMLDivElement>();
-
-  useEffect(() => {
-    const anim = lottie.loadAnimation({
-      container: animationContainer.current!,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      animationData: LOTTIE,
-    });
-
-    const lottieShow = () => {
-      setShowLottie(true);
-    };
-
-    anim.addEventListener('DOMLoaded', lottieShow);
-
-    return () => {
-      anim.destroy();
-    };
-  }, [animationContainer]);
 
   const closeModal = () => {
     setModalOpen(false);
@@ -94,21 +71,7 @@ export default function StagingDisplay({
           }}
         />
       </Modal>
-      {(!originalImage || mode) && renders.length === 0 && (
-        <div className={styles.displayAnimation}>
-          <div ref={animationContainer}></div>
-          <div
-            className={`${styles.pitch} ${showLottie && styles.pitchVisible}`}
-          >
-            Experience the power of AI-generated virtual staging images, and
-            save up to 95% compared to traditional virtual staging agencies who
-            bill up to $60 per image and may take up to 3 days to complete the
-            work. With our AI technology, you&apos;ll receive high-quality virtual
-            staging images in seconds at a fraction of the cost. Don&apos;t wait -
-            start saving time and money today!
-          </div>
-        </div>
-      )}
+      {(!originalImage || mode) && renders.length === 0 && <EmptyDisplay />}
       {!mode && originalImage && (
         <Sketch originalImage={originalImage} sketchRef={sketchRef} />
       )}
