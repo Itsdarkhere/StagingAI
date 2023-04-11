4537f8be29b19269f0f61fd636b996d4ffeacc8b
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-04-11 13:33:59 +0300

    Logo changes

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -19,6 +19,7 @@ export default function SideNav() {
         <div className={styles.top}>
           <Link href="/" className={styles.logolink}>
             <Image height={40} src={sofa} alt="sofa" />
+            Realtool
           </Link>
           <div className={styles.bio}>
             <h6 className={styles.name}>Valtteri Juvonen</h6>
