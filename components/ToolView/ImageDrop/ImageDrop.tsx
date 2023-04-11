80b748e4802b4f002a04cc23267f70361a5fcf5b
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-04-11 12:51:16 +0300

    Move formdropzone into imageDrop

--- components/ToolView/ImageDrop/ImageDrop.tsx
+++ components/ToolView/ImageDrop/ImageDrop.tsx
@@ -1,6 +1,9 @@
-import FormDropZone from '@/components/FormDropZone';
+import { Button } from "@mui/material"
+import Image from 'next/image';
 import styles from '../../../styles/ToolView/ImageDrop/ImageDrop.module.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import IMGUP from "../../../public/imageup.svg";
+import Spinner from "@/components/Spinner";
 
 export default function ImageDrop({
   originalImage,
@@ -11,6 +14,11 @@ export default function ImageDrop({
 }) {
   const [uploadingPhoto, setUploadingPhoto] = useState(false);
   const [dragActive, setDragActive] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [originalImage]);
 
   // Remove image from state
   const removeImage = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -80,15 +88,63 @@ export default function ImageDrop({
   return (
     <>
       <div className={styles.container} onDragEnter={handleDrag}>
-        <FormDropZone
-          uploadingPhoto={uploadingPhoto}
-          image={originalImage}
-          removeImage={removeImage}
-          handleChange={handleChange}
-          dragActive={dragActive}
-          handleDrag={handleDrag}
-          handleDrop={handleDrop}
-        />
+        <label
+        htmlFor="input-file-upload"
+        className={`${styles.inputLabel} ${dragActive && styles.drag_active}`}
+      >
+        <div className={styles.inputLabelInner}>
+          {!uploadingPhoto && originalImage !== undefined && (
+            <Image
+              fill
+              onLoad={() => setLoaded(true)}
+              sizes="400px"
+              style={{ objectFit: 'cover' }}
+              className={`${styles.image} ${loaded && styles.loaded}`}
+              src={originalImage + '?' + Math.random()}
+              alt="upload"
+            />
+          )}
+          {uploadingPhoto || (originalImage !== undefined && !loaded) && <Spinner wh={45} white={true} />}
+
+          {originalImage === undefined && !uploadingPhoto && (
+            <>
+              <Image height={75} src={IMGUP} alt="upload" />
+              <p className={styles.p}>
+                Drag your image here to start uploading
+              </p>
+              <p className={styles.span}>───── OR ─────</p>
+              <Button variant="contained" 
+              component="label"
+              style={{marginTop: 15, textTransform: 'none', 
+              backgroundColor: 'rgb(99, 102, 241)', height: 40,
+              paddingLeft: 25, paddingRight: 25}}>Browse files
+                <input hidden accept="image/*" multiple={false} onChange={handleChange} type="file"
+                name="input-file-upload"
+                required={originalImage ? false : true} />
+              </Button>
+            </>
+          )}
+
+          {!uploadingPhoto && loaded && (
+            <button
+              type="button"
+              onClick={removeImage}
+              className={styles.closeButton}
+            >
+              X
+            </button>
+          )}
+        </div>
+      </label>
+      {dragActive && (
+        <div
+          className={styles.dragFileElement}
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        ></div>
+      )}
       </div>
     </>
   );
